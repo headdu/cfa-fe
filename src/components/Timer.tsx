f346ed0640fcce11587f2ed2e3a469b186ee68f3
Eduardo Martins <eduardomartins@Eduardos-MacBook-Pro.local>
2020-04-03 10:09:25 +0100

    Only sync once

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -12,11 +12,13 @@ export default function Timer({
   isAdmin: boolean;
 }) {
   const [start, setStart] = React.useState(Date.now()+1000);
+  const [synced, setSynced] = React.useState(false)
   const [content, setContent] = React.useState(time);
 
 
   React.useEffect(() => {
     setStart(Date.now());
+    setSynced(false);
   }, [label, time]);
 
   // we don't want to wait a full second before the timer starts
@@ -33,12 +35,12 @@ export default function Timer({
         seconds = diff % 60;
         setContent(seconds);
       }, 50);
-    } else {
-      if (isAdmin) {
-        sync();
-      }
+    } else if (!synced && isAdmin) {
+      setSynced(true);
+      sync();
+      
     }
-  }, [time, start, isAdmin, content]);
+  }, [time, start, isAdmin, content, synced]);
 
 
   return (
