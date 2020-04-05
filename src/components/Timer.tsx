b28ea2c23b3bff86703f1d2a87eb7b87c2d8096a
Eduardo Martins <eduardomartins@Eduardos-MacBook-Pro.local>
2020-04-05 19:40:55 +0100

    Refactor timer to simple settimeout

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,75 +1,96 @@
-import React, { Component } from "react";
+import React from "react";
 import { sync } from "../api";
 import { Flex } from "rebass";
+
+
+
+const isToBeep = (actualSeconds: number) => {
+  return [3, 2, 1].includes(actualSeconds);
+};
+
+const getCountdownString = (timeLeft: number) => {
+  let countdownString = "";
+  const minutes = Math.floor(timeLeft / 60);
+  if (minutes) {
+    if (minutes < 10) {
+      countdownString = countdownString + `0${minutes}:`
+    } else {
+      countdownString = countdownString + `${minutes}:`
+    }
+  }
+
+  const seconds = Math.round(timeLeft % 60);
+
+  if (seconds !== 60) {
+    if (seconds < 10) {
+      countdownString = countdownString + `0${seconds}`;
+    } else {
+      countdownString = countdownString + seconds;
+    }
+  } else {
+    countdownString = countdownString + '00'
+  }
+
+  return countdownString;
+};
 
 export default function Timer({
   label,
   time,
-  isAdmin
+  isAdmin,
 }: {
   label: string;
   time: number;
   isAdmin: boolean;
 }) {
-  const [start, setStart] = React.useState(Date.now()+1000);
-  const [synced, setSynced] = React.useState(false)
-  const [content, setContent] = React.useState(time + 1);
-  const beepSound = document.getElementById("beep") as HTMLAudioElement;
-
-  const isToBeep = (actualSeconds:number) => {
-    return (
-      Math.round(actualSeconds) > 0 &&
-      Math.round(actualSeconds) <= 3 &&
-      Math.round(content) !== Math.round(actualSeconds)
-    );
-  }
-
-  const playBeep = () => {
-    if (beepSound) {
-      beepSound.load();
-      const promise = beepSound.play()
-        .catch((error) =>{
-          //safari browser sometimes throws a exception when play the sound (poop)
-          //must catch it to not break anything!
-          console.error(error)
-        });
-    }
-  }
+  const [startDate, setStart] = React.useState<any>(null);
+  const [synced, setSynced] = React.useState(false);
+  const [content, setContent] = React.useState(time);
 
   React.useEffect(() => {
-    setStart(Date.now());
+    setStart(null);
     setSynced(false);
+    setContent(time);
   }, [label, time]);
 
   // we don't want to wait a full second before the timer starts
 
   React.useEffect(() => {
-    let diff: number, seconds;
-    diff = time - (Date.now() - start) / 1000;
-    if (Number.parseFloat(diff.toFixed(1)) > 0.5) {
+    const newContent = content - 1
+    const beepSound = document.getElementById("beep") as HTMLAudioElement;
+
+    const playBeep = () => {
+      if (beepSound) {
+        beepSound.load();
+        beepSound.currentTime = 0;
+        beepSound.play().catch((error) => {
+          //safari browser sometimes throws a exception when play the sound (poop)
+          //must catch it to not break anything!
+          console.error(error);
+        });
+      }
+    };
+    if (content > 0) {
       setTimeout(() => {
         // get the number of seconds that have elapsed since
         // startTimer() was called
 
         // does the same job as parseInt truncates the float
-        seconds = diff % 60;
-        if(isToBeep(seconds)) {
+        if (isToBeep(newContent)) {
           playBeep();
         }
-        setContent(seconds);
-      }, 50);
+        setContent(newContent);
+      }, 999);
     } else if (!synced && isAdmin) {
       setSynced(true);
       sync();
-
     }
-  }, [time, start, isAdmin, content, synced]);
-
+  }, [time, startDate, isAdmin, content, synced]);
 
   return (
     <Flex flexDirection="column" alignItems="center">
       <h1>{label}</h1>
-      <h1>{Math.round(content)}</h1>
+      <h1>{getCountdownString(content)}</h1>
     </Flex>
   );
 }
