e7ba91fa758ad1fb11e66bd7b4790975dd4189c6
Eduardo Martins <eduardomartins@Eduardos-MacBook-Pro.local>
2020-04-05 16:56:04 +0100

    Add method to update config

--- src/api.tsx
+++ src/api.tsx
@@ -1,118 +1,138 @@
 const socket = new WebSocket("wss://cf-aveiro.herokuapp.com");
 
+export interface ConfigItem {
+  label: string;
+  seconds: number;
+  type: string;
+}
+
 export function createRoom() {
   socket.send(JSON.stringify({ type: "create" }));
 }
 
 export function joinRoom(uuid: string) {
-  socket.send(JSON.stringify({type: 'join', uuid}))
+  socket.send(JSON.stringify({ type: "join", uuid }));
+}
+
+export function setConfig(config: ConfigItem[]) {
+  socket.send(
+    JSON.stringify({
+      type: "config",
+      config
+    })
+    )
 }
 
 export function setTabata() {
-  
   socket.send(
     JSON.stringify({
       type: "config",
       config: [
         {
           label: "Countdown",
-          seconds: 10000
+          seconds: 10000,
         },
         {
           label: "GO!",
-          seconds: 20000
+          seconds: 20000,
         },
         {
           label: "Rest",
-          seconds: 10000
+          seconds: 10000,
         },
         {
           label: "GO!",
-          seconds: 20000
+          seconds: 20000,
         },
         {
           label: "Rest",
-          seconds: 10000
+          seconds: 10000,
         },
         {
           label: "GO!",
-          seconds: 20000
+          seconds: 20000,
         },
         {
           label: "Rest",
-          seconds: 10000
+          seconds: 10000,
         },
         {
           label: "GO!",
-          seconds: 20000
+          seconds: 20000,
         },
         {
           label: "Rest",
-          seconds: 10000
+          seconds: 10000,
         },
         {
           label: "GO!",
-          seconds: 20000
+          seconds: 20000,
         },
         {
           label: "Rest",
-          seconds: 10000
+          seconds: 10000,
         },
         {
           label: "GO!",
-          seconds: 20000
+          seconds: 20000,
         },
         {
           label: "Rest",
-          seconds: 10000
+          seconds: 10000,
         },
         {
           label: "GO!",
-          seconds: 20000
+          seconds: 20000,
         },
         {
           label: "Rest",
-          seconds: 10000
+          seconds: 10000,
         },
         {
           label: "GO!",
-          seconds: 20000
+          seconds: 20000,
         },
         {
           label: "Rest",
-          seconds: 10000
-        }
-      ]
+          seconds: 10000,
+        },
+      ],
     })
   );
 }
 
 export function setRest() {
-  socket.send(JSON.stringify({
-    type: 'config',
-    config: [
-      {
-        label: 'Rest',
-        seconds: 60000
-      }
-    ]
-  }))
+  socket.send(
+    JSON.stringify({
+      type: "config",
+      config: [
+        {
+          label: "Rest",
+          seconds: 60000,
+        },
+      ],
+    })
+  );
 }
 
 export function sync() {
-  socket.send(JSON.stringify({
-    type: 'sync'
-  }))
+  socket.send(
+    JSON.stringify({
+      type: "sync",
+    })
+  );
 }
 
 export function ping() {
   setInterval(() => {
-    socket.send(JSON.stringify({
-      type: 'ping'
-    }))
-  } , 5000)
+    socket.send(
+      JSON.stringify({
+        type: "ping",
+      })
+    );
+  }, 5000);
 }
 
-socket.addEventListener('open', ping)
+socket.addEventListener("open", ping);
 
-export default socket
+export default socket;
